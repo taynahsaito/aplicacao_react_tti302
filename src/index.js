29f4430785bd78e9b82da87a8171ce8f7f0f3c81
TAYNAH TAMIE MASUNAGA SAITO <23.00621-8@maua.br>
2025-10-14 22:19:18 -0300

    aula do dia 14/10

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//carregando as funções dos pacotes dentro de variáveis
+import React from 'react'
+//cria a raiz da aplicação, que se refere ao ponto da árvore DOM onde o código de <App/> (aplicação como um todo) será carregado
+import {createRoot} from 'react-dom/client' 
+ 
+//constante app vai guardar uma função e essa função vai retornar o valor dentro das divs
+//poderia ser uma let, mas para ocupar menos memoria, o ideal é colocar const já que o valor não irá mudar
+const App = () => {
+    //JSX - linguagem que mistura javascript com HTML 
+    return <div> Meu primeiro componente React </div>
+}
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+//render = renderizar/construir a interface gráfica a partir do componente que representa a interface como um todo
+// ReactDOM.render(
+//     <App/>, //para ser visto como um componente pelo react, passamos ele como uma tag
+//     //document = árvore HTML
+//     //querySelector busca entre todas as tags HTML qual é a referente ao valor que coloquei dentro do ()
+//     document.querySelector("#root") //# = estou especificando que é um valor referente a ID (id=root)
+// )
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//usamos essa pois o bloco de codigo anterior comentado foi deprecated
+const root = createRoot(document.getElementById("root"))
+root.render(<App/>)
